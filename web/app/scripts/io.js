e972b3a1d3e009c93c931e92cf2142a9e9c9dd2a
Yang Su <suyang1+services@gmail.com>
2012-10-06 14:52:10 -0400

    aeou

--- web/app/scripts/io.js
+++ web/app/scripts/io.js
@@ -1,6 +1,6 @@
 var socket = io.connect();
 socket.on('new_content', function (data) {
   FBHack.router.view.model.add({ src: data.payload.link }).trigger('change');
-  var offset =  FBHack.router.view.$el.find('.box:last-child').offset().left;
-  $('div').scrollLeft(FBHack.router.view.$el.find('#stream_container').width());
+  var offset =  FBHack.router.view.$el.find('.box:last-child').width();
+  $('div').scrollLeft(FBHack.router.view.$el.find('#stream_container').width() + offset);
 });
