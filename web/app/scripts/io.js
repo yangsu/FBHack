0e548aaab5242107d54e7df5ffdfa70a21f613e9
Yang Su <suyang1+services@gmail.com>
2012-10-06 14:38:31 -0400

    css changes

--- web/app/scripts/io.js
+++ web/app/scripts/io.js
@@ -1,7 +1,9 @@
 var socket = io.connect();
 socket.on('new_content', function (data) {
   FBHack.router.view.model.add({ src: data.payload.link }).trigger('change');
+  var offset = FBHack.router.view.$el.find('.box:last-child').width()
+              + FBHack.router.view.$el.find('.box:last-child').offset().left;
   $('#stream, #stream_container').css({
-    'margin-left': (FBHack.router.view.maxPos - 200) + 'px'
+    'margin-left': -offset + 'px'
   });
 });
