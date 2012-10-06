5bc73093cdfc935650a40c2f92745bd486973d52
Yang Su <suyang1+services@gmail.com>
2012-10-06 13:53:42 -0400

    stuff

--- web/app/scripts/views/Stream-view.js
+++ web/app/scripts/views/Stream-view.js
@@ -7,14 +7,15 @@ FBHack.Views.StreamView = Backbone.View.extend({
     this.model.on('add', this.render, this);
   },
   events: {
-    'click #qr': 'showQR'
+    'click #qrlink': 'showQR'
   },
-  showQR: function () {
+  showQR: function (e) {
     $('#qr')
       .find('.content')
       .html(this.qrTemplate({ id : this.model.get('id') }))
       .end()
     .show('slow');
+    e.preventDefault();
   },
   fetchNext: function () {
     this.model.fetchNext();
