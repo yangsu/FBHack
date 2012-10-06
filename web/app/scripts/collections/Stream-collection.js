9ee20a542766ff2d5d1e6175dab5096d5a2eaaf2
Yang Su <suyang1+services@gmail.com>
2012-10-06 06:27:51 -0400

    Removed unnecessary styles

--- web/app/scripts/collections/Stream-collection.js
+++ web/app/scripts/collections/Stream-collection.js
@@ -6,27 +6,27 @@ FBHack.Collections.StreamCollection = Backbone.Collection.extend({
   // dimensions: [25, 30, 40, 50, 60, 70, 75, 100],
   dimensions: [30, 40, 50, 60, 70, 100],
   getRandomDimension: function () {
-    return this.dimensions[Math.floor(Math.random()*this.dimensions.length)]
+      return this.dimensions[Math.floor(Math.random()*this.dimensions.length)]
   },
   computeLayout: function (width, height) {
-      var i, w, h, left, top,
+      var i, img, w, h, left, top,
 	left1 = 0, left2 = 0,
 	lastTopH = 0, lastBotH = 0,
 	lastTopW = 0, lastBotW = 0,
-	diff = 0, border;
+	diff = 0, border, scalar;
       for (i = 0; i < this.length; i++) {
+	  img = this.at(i);
 	  w = this.getRandomDimension();
 	  border = '';
 	  if (left1 > left2) {
 	      border = 'border-t';
 	      left = left2;
 	      diff = left1 - left2;
-	      if (Math.random() >= 0.5 && diff >= 25 && _.contains(this.dimensions, diff)) {
+	      if (Math.random() >= 0.5 && diff >= 25) {
 		w = diff;
 		left2 = left1;
 	      } else {
 		left2 += w;
-		// border += ' border-r';
 	      }
 	      top = lastTopH;
 	      h = 100 - lastTopH;
@@ -35,19 +35,17 @@ FBHack.Collections.StreamCollection = Backbone.Collection.extend({
 	  } else if (left2 > left1) {
 	      left = left1;
 	      diff = left2 - left1;
-	      if (Math.random() >= 0.5 && diff >= 25 && _.contains(this.dimensions, diff)) {
+	      if (Math.random() >= 0.5 && diff >= 25) {
 		w = diff;
 		left1 = left2;
 	      } else {
 		left1 += w;
-		// border = ' border-r';
 	      }
 	      top = 0;
 	      h = 100 - lastBotH;
 	      lastTopH = h;
 	      lastTopW = w;
 	  } else {
-	      // border = ' border-r';
 	      left = left1;
 	      left1 += w;
 	      top = 0;
@@ -62,7 +60,7 @@ FBHack.Collections.StreamCollection = Backbone.Collection.extend({
 	    t: top,
 	    border: border
 	  };
-	  this.at(i).set(vals);
+	  img.set(vals);
 	}
     return this;
   }
